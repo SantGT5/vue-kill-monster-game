53e5ae4571b8bbef01cd229cb566970570f704dc
SantGT5 <gianspf@gmail.com>
2022-04-10 18:13:34 +0200

    Result winner or loser

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-new Vue({
-  el: "#app",
-  data: {},
-  computed: {},
-  methods: {},
-  watch: {},
-});
+Vue.createApp({
+  data() {
+    return {
+      playerLife: 0,
+      monsterLife: 20,
+    };
+  },
+  computed: {
+    hasResult() {
+      return this.playerLife === 0 || this.monsterLife === 0;
+    },
+  },
+}).mount("#app");
