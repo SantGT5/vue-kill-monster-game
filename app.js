68f5b0ac58cfe0a38ff44b0c33933d0ba5444532
SantGT5 <gianspf@gmail.com>
2022-04-11 15:17:47 +0200

    Heal Btn && Logs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ Vue.createApp({
       running: false,
       playerLife: 100,
       monsterLife: 100,
+      logs: [],
     };
   },
   computed: {
@@ -16,23 +17,58 @@ Vue.createApp({
       this.running = true;
       this.playerLife = 100;
       this.monsterLife = 100;
+      this.logs = [];
     },
     attack(especial) {
-      this.hurt("monsterLife", 5, 10, especial);
-      this.hurt("playerLife", 7, 12, false);
+      this.hurt(
+        "monsterLife",
+        5,
+        10,
+        especial,
+        "Player",
+        "Monster",
+        "playerCls"
+      );
+
+      if (this.monsterLife > 0) {
+        this.hurt(
+          "playerLife",
+          7,
+          12,
+          false,
+          "Monster",
+          "Player",
+          "monsterCls"
+        );
+      }
     },
-    hurt(prop, min, max, especial) {
+    hurt(prop, min, max, especial, source, target, cls) {
       const plus = especial ? 5 : 0;
       const hurt = this.getRandom(min + plus, max + plus);
 
       this[prop] = Math.max(this.playerLife - hurt, 0);
+
+      this.registerLog(`${source} hits ${target} with ${hurt}.`, cls);
+    },
+    healAndHurt() {
+      this.heal(10, 15);
+      this.hurt("playerLife", 7, 12, false, "Monster", "Player", "monsterCls");
+    },
+    heal(min, max) {
+      const heal = this.getRandom(min, max);
+      this.playerLife = Math.min(this.playerLife + heal, 100);
+      this.registerLog(`Payer healed ${heal}`, "playerCls");
     },
     getRandom(min, max) {
       const value = Math.random() * (max - min) + min;
 
       return Math.round(value);
     },
+    registerLog(text, cls) {
+      this.logs.unshift({ text, cls });
+    },
   },
+
   watch: {
     hasResult(value) {
       if (value) this.running = false;
